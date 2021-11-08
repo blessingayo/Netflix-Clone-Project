35cccf89103e0f079db881e01dee069c8936d802
Blessing Asuelimen <asuelimenblessing630@gmail.com>
2021-11-08 10:03:04 -0800

    Netflix-Clone-Project

---
+++ src/Banner.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+
+function Banner() {
+
+    
+const [movie, setMovie] = useState([]);
+
+useEffect(() => {
+
+}, []);
+
+  return(
+   <header>
+
+  </header>
+  );
+}
+export default Banner;
---
+++ src/requests.js
@@ -0,0 +1,13 @@
+const API_KEY = "4a4b05899b95ed94b04c90fa30105911";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+export default requests;
